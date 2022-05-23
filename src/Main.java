9344a4245e3c789bba1eec2d9289ceaacb08ab79
Kristina Artyushechkina <104067981+FairyGreta@users.noreply.github.com>
2022-05-23 12:48:50 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         int account = 100; // начальный счёт
-        int x = 2000; // сумма пополнения
+        int money = 2000; // сумма пополнения
 
         int bonus;
-        if (x > 1_000) {
-            bonus = x * 1 / 100; // бонус = по одному рублю за каждые 100 рублей
+        if (money > 1_000) {
+            bonus = money * 1 / 100; // бонус = по одному рублю за каждые 100 рублей
         } else {
             bonus = 0;
         }
 
-        int totalAccount = account + x + bonus;
+        int totalAccount = account + money + bonus;
         System.out.println("Итоговая сумма: " + totalAccount + ", из них бонус " + bonus);
     }
 }
